513fda1e1500c6a1a0a8081dd5ca93054538eacc
Jim <jshaun1989@hotmail.co.uk>
2021-04-13 22:37:46 +0100

    Initial Commit and Custom User Model

---
+++ WatchTower_V1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WatchTower_V1.Models;
+
+namespace WatchTower_V1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<UserModel>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WatchTower_V1/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchTower_V1.Models
+{
+    public class UserModel: IdentityUser
+    {
+        [Required]
+        public string Fname { get; set; }
+
+        [Required]
+        public string SName { get; set; }
+
+
+    }
+}
